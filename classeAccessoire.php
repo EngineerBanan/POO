444d8fcd6467e85314f39dbd79ba0ab1ed446a7b
EngineerBanan <legrand.gatien77@gmail.com>
2025-09-29 01:00:02 +0200

    repo pour mon dossier POO

---
+++ classeAccessoire.php
@@ -0,0 +1,9 @@
+<?php
+class Accessoire {
+    public $nom;
+    public $prix;
+    public $temperature;
+    public $image;
+    public $image_MES;
+}
+?>
---
+++ classePlante.php
@@ -0,0 +1,8 @@
+<?php
+class Plante {
+    public $nom;
+    public $prix;
+    public $temperature;
+    public $image;
+}
+?>
